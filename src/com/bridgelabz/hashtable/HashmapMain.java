758405605cba7d385de1d5a5167402249df48d9b
Pallavic5 <pchedge235@gmail.com>
2022-10-03 14:58:42 +0530

    Added: Ability to find frequency of word in sentence

---
+++ src/com/bridgelabz/hashtable/HashmapMain.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.hashtable;
+
+public class HashmapMain {
+
+	public static void main(String[] args) {
+
+		HashmapMain main = new HashmapMain();
+		main.givenSentence_WhenWordsAreAddedToList_ShouldReturnWordFrequency();
+
+	}
+
+	public void givenSentence_WhenWordsAreAddedToList_ShouldReturnWordFrequency() {
+		String sentence = "To Be or not To be";
+		MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value += 1;
+			myHashMap.add(word, value);
+		}
+		System.out.println(myHashMap);
+		int frequency = myHashMap.get("to");
+		System.out.println("frequency = " + frequency);
+	}
+
+}
