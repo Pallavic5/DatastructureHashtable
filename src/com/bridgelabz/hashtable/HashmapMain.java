a681bd3911d5b45d2b1f60f42d016c19ea41bf38
Pallavic5 <pchedge235@gmail.com>
2022-10-03 15:18:43 +0530

    Added: Ability to find frequency of words in a large paragraph phrase

--- src/com/bridgelabz/hashtable/HashmapMain.java
+++ src/com/bridgelabz/hashtable/HashmapMain.java
@@ -6,6 +6,7 @@ public class HashmapMain {
 
 		HashmapMain main = new HashmapMain();
 		main.givenSentence_WhenWordsAreAddedToList_ShouldReturnWordFrequency();
+		main.givenSentence_whenWordsAreAddedToList_shouldReturnParanoidWordFrequency();
 
 	}
 
@@ -19,10 +20,26 @@ public class HashmapMain {
 				value = 1;
 			else
 				value += 1;
-			myHashMap.add(word, value);
+				myHashMap.add(word, value);
 		}
 		System.out.println(myHashMap);
 		int frequency = myHashMap.get("to");
+		System.out.println("frequency = " + frequency);
+	}
+
+	public void givenSentence_whenWordsAreAddedToList_shouldReturnParanoidWordFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value += 1;
+			myLinkedHashMap.add(word, value);
+		}
+		int frequency = myLinkedHashMap.get("paranoid");
 		System.out.println("frequency = " + frequency);
 	}
 
